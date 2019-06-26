d6d002f600daaff6e017286ca94719a03360629f
emark1 <devel010@umn.edu>
2019-06-26 16:12:22 -0500

    Adding React production build

---
+++ client/build/precache-manifest.91ad3aa9bd80d612cc0328224911a794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800fa9f942f044d4f40c6d8478b8b016",
+    "url": "/index.html"
+  },
+  {
+    "revision": "916ec8e9ac0f68a58d41",
+    "url": "/static/css/2.3ab75c30.chunk.css"
+  },
+  {
+    "revision": "f0a108ccca2610d5ffb8",
+    "url": "/static/css/main.b3c10fb4.chunk.css"
+  },
+  {
+    "revision": "916ec8e9ac0f68a58d41",
+    "url": "/static/js/2.a585ca06.chunk.js"
+  },
+  {
+    "revision": "f0a108ccca2610d5ffb8",
+    "url": "/static/js/main.ae168820.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
